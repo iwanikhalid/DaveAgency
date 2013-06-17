3165790cbcb00936b214481062d4a7e625804d56
Iwani Khalid <iwanikhalid@gmail.com>
2013-06-17 08:44:51 +0800

    Added links, corrected title

--- js/fx.js
+++ js/fx.js
@@ -1,4 +1,20 @@
-
+$(function() {
+      $('#slides').slidesjs({
+        width: 950,
+        height: 367,
+		pagination: false,
+        generatePagination: false,
+		navigation: false,
+        generateNavigation: false,		
+        play: {
+          active: true,
+          auto: true,
+          interval: 5000,
+		  effect: "fade",
+          swap: true
+        }
+      });
+    });
 
 
 jQuery(document).ready(function($) {
